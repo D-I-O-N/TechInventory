e7a18bfa97058e14018da1ef77a8c828e4b34905
D-I-O-N <136984903+D-I-O-N@users.noreply.github.com>
2024-07-21 00:59:25 +0300

    Add files via upload

---
+++ _src/pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TechInventory._src.database;
+using TechInventory._src.pages.statistics;
+
+namespace TechInventory._src.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        Entities entities = new Entities();
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            // Получаем значения из текстовых полей
+            string username = userName.Text;
+            string password = userPassword.Password;
+
+            // Проверяем имя пользователя и пароль в базе данных
+            var user = entities.Users.FirstOrDefault(u => u.Name == username && u.Password == password);
+
+            if (user != null)
+            {
+                // Определяем роль пользователя
+                switch (user.Urole)
+                {
+                    case "Admin":
+                        // Перенаправляем на страницу администратора
+                        NavigationService.Navigate(new MainPage());
+                        break;
+
+                    case "User":
+                        // Перенаправляем на страницу пользователя
+                        NavigationService.Navigate(new PageStatisticReport());
+                        break;
+
+                    default:
+                        MessageBox.Show("Неизвестная роль пользователя.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Неверное имя пользователя или пароль.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ _src/pages/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms.Integration;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TechInventory._src.pages.about;
+using TechInventory._src.pages.employees;
+using TechInventory._src.pages.equipment;
+using TechInventory._src.pages.equipmentHistory;
+using TechInventory._src.pages.rooms;
+using TechInventory._src.pages.statistics;
+
+namespace TechInventory._src.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PageRoomsOpen_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RoomsPage());
+        }
+
+        private void PageEmployeeOpen_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new EmployeesPage());
+        }
+
+        private void PageEquipmentOpen_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new EquipmentPage());
+        }
+
+        private void PageEquipmentHistoryOpen_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new EquipmentHistoryPage());
+        }
+
+        private void OpenStatic_Click(object sender, RoutedEventArgs e)
+        {
+            //NavigationService.Navigate(new EquipmentReportPage());
+            NavigationService.Navigate(new PageStatisticReport());
+        }
+
+        private void OpenAbout_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AboutPage());
+            
+        }
+    }
+}
